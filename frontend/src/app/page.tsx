2535bb6fb0ce664c63b25f63327b0824cac68e39
abhithory <abhithory@gmail.com>
2023-07-09 14:54:30 +0530

    basic 2 buttons added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,11 +14,11 @@ export default function Home() {
         <h1>Tagline</h1>
         <h1>DataVault</h1>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus autem quod, voluptate harum pariatur possimus nobis necessitatibus, dolorum alias temporibus, cumque molestiae provident id ratione. Illo tenetur mollitia ducimus voluptatem labore natus.</p>
-        <div className="flex">
-        <button onClick={()=>{
+        <div className="flex gap-4 mt-4">
+        <button className="btn_1" onClick={()=>{
           router.push("/app")
         }}>Launch dApp</button>
-        <button>Learn More</button>
+        <button className="btn_2">Learn More</button>
         </div>
       </section>
       <section className="basis-1/2">
