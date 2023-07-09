78fae96b2a3670565edc5a60355b11b64ea9307a
abhithory <abhithory@gmail.com>
2023-07-09 17:49:04 +0530

    two navigation added and created diffrent layouts

--- frontend/src/components/Sections/Nav/Navigation.tsx
+++ frontend/src/components/Sections/Nav/Navigation.tsx
@@ -1,7 +1,5 @@
 "use client"
-
 import React from 'react'
-import { ConnectWallet } from "@thirdweb-dev/react";
 import Image from 'next/image'
 
 import { usePathname, useRouter } from 'next/navigation';
@@ -9,22 +7,22 @@ import { FaLandmark } from 'react-icons/fa';
 
 
 function Navigation() {
-  const  router = useRouter();
+  const router = useRouter();
   const pathname = usePathname();
 
-  if (pathname.startsWith("/match/")) {
+  if (pathname.startsWith("/app/")) {
     return <></>
   }
   return (
     <nav className="glassmorphism-bg w-full flex justify-between px-20 py-4 h-[12vh]">
-      <span onClick={()=>router.push("/")} className=" cursor-pointer flex items-center justify-center gap-4">
+      <span onClick={() => router.push("/")} className=" cursor-pointer flex items-center justify-center gap-4 ml-8">
         <Image
           src="/assests/logo.svg"
-          width={80}
-          height={80}
+          width={100}
+          height={100}
           alt="Picture of the author"
         />
-        <h1 className="text-2xl text-text-color ">Data Vault</h1>
+        {/* <h1 className="text-2xl text-text-color ">Data Vault</h1> */}
       </span>
       <span className="flex items-center justify-center gap-4">
         {/* <h1 className="basic_btn_4" onClick={()=>router.push("/profile")}>
@@ -36,8 +34,10 @@ function Navigation() {
           <FaLandmark  className=''/>
           Marketplace
           </h1> */}
+        <button className="btn_primary_2" onClick={() => {
+          router.push("/app")
+        }}>Launch dApp</button>
 
-        <ConnectWallet />
       </span>
     </nav>
   )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,8 +9,10 @@ module.exports = {
     extend: {
       colors:{
         'primary':"#4D92FF", //pink-500
+        'primary-hover':"#2372f1", //pink-500
         'primary-bg':"#010220", //#09162e
-        'secondary':"#BB65FF",
+        'secondary':"rgb(147 51 234)", // 
+        'secondary-hover':"#a64ef5", // 
         'third':"#5E17EB",
         'highlight':"#FCB040",
         'text-color':"#FFFFFF",
