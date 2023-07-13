603d9a0bd72472c3d7ed22195c9a8a72fc9498b0
abhithory <abhithory@gmail.com>
2023-07-13 07:18:12 +0530

    smart contract upgraded

--- smart contract/contracts/DataVault.sol
+++ smart contract/contracts/DataVault.sol
@@ -4,103 +4,53 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract DataVault {
-    // structure of file data
+    enum DataType{
+        CREDENTIALS,
+        FILE,
+        INFO,
+        OTHER
+    }
 
-    // struct ScreatItemStruct {
-    //     string name;
-    //     string fileHash;
-    //     string decryptKey;
-    //     uint256 uploadTime;
-    // }
-
-    struct FileStruct {
-        string fileName;
+    struct DataStruct {
+        DataType dataType;
+        string name;
         string fileHash;
         string decryptKey;
+        uint256 uploadTime;
     }
 
-    // structure of Credential data
-    struct CredentialStruct {
-        string website;
-        string usernameOrEmailOrPhone;
-        string password;
+    mapping(address => DataStruct[]) private _allDataOfUser;
+    event DataAdded(DataType indexed dataType,address indexed user, string fileHash,string decryptKey,uint256 uploadTime);
+
+
+    function addData(DataStruct memory _data) external{
+        _allDataOfUser[msg.sender].push(_data);
+    }
+    function userAllDataCount() public view returns (uint256) {
+        return _allDataOfUser[msg.sender].length;
     }
 
-    // event FileAddedByUser(string name,address indexed user);
-    // event CredentialAddedByUser(string name,address indexed user);
-
-    // mapping of all files of user
-    mapping(address => FileStruct[]) private _allFilesOfUser; // addressOfUser => files[]
-
-    //  mapping of all credentials of user
-    mapping(address => CredentialStruct[]) private _allCredentialsOfUser; //addressOfUser => credentials[]
-
-    mapping(address => FileStruct[]) private _allSecretInfo; //addressOfUser => credentials[]
-
-    // add data of file of user
-    function addFileOfUser(FileStruct memory _fileData) external {
-        _allFilesOfUser[msg.sender].push(_fileData);
-    }
-
-    // add credential of user
-    function addScreatInfoOfUser(FileStruct memory _scretInfo) external {
-        _allSecretInfo[msg.sender].push(_scretInfo);
-    }
-
-    function addCredentialOfUser(
-        CredentialStruct memory _credentialData
-    ) external {
-        _allCredentialsOfUser[msg.sender].push(_credentialData);
-    }
-
-    // get total files count that user added
-    function userTotalFilesCount() internal view returns (uint256) {
-        return _allFilesOfUser[msg.sender].length;
-    }
-
-    // get total files count that user added
-    function userTotalScreatInfoCount() internal view returns (uint256) {
-        return _allSecretInfo[msg.sender].length;
-    }
-
-    // get total credentials count that user added
-    function userTotalCredentialCount() internal view returns (uint256) {
-        return _allCredentialsOfUser[msg.sender].length;
-    }
-
-    function getAllFilesOfUser()
+    function getAllData()
         external
         view
-        returns (FileStruct[] memory allFiles)
+        returns (DataStruct[] memory allData)
     {
-        uint256 totalFiles = userTotalFilesCount();
-        allFiles = new FileStruct[](totalFiles);
-        for (uint i = 0; i < totalFiles; i++) {
-            allFiles[i] = _allFilesOfUser[msg.sender][i];
+        uint256 totalData = userAllDataCount();
+        allData = new DataStruct[](totalData);
+        for (uint i = 0; i < totalData; i++) {
+            allData[i] = _allDataOfUser[msg.sender][i];
         }
     }
 
-    function getAllCredentialsOfUser()
-        external
-        view
-        returns (CredentialStruct[] memory allCreds)
-    {
-        uint256 totalCredentials = userTotalCredentialCount();
-        allCreds = new CredentialStruct[](totalCredentials);
-        for (uint i = 0; i < totalCredentials; i++) {
-            allCreds[i] = _allCredentialsOfUser[msg.sender][i];
+    function userSpecifcDataCount(DataType _dataType) public view returns (uint256) {
+        uint256 totalData = userAllDataCount();
+        uint256 _specificDataCount = 0;
+        for (uint i = 0; i < totalData; i++) {
+            if(_allDataOfUser[msg.sender][i].dataType == _dataType){
+             _specificDataCount++;   
+            }
         }
+        return _specificDataCount;
     }
 
-    function getAllScreatInfoOfUser()
-        external
-        view
-        returns (FileStruct[] memory allInfos)
-    {
-        uint256 totalScreatInfo = userTotalScreatInfoCount();
-        allInfos = new FileStruct[](totalScreatInfo);
-        for (uint i = 0; i < totalScreatInfo; i++) {
-            allInfos[i] = _allSecretInfo[msg.sender][i];
-        }
-    }
 }
---
+++ smart contract/contracts/OldDataVault.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// import "hardhat/console.sol";
+
+contract OldDataVault {
+    // structure of file data
+
+    struct FileStruct {
+        string fileName;
+        string fileHash;
+        string decryptKey;
+    }
+
+    // structure of Credential data
+    struct CredentialStruct {
+        string website;
+        string usernameOrEmailOrPhone;
+        string password;
+    }
+
+    // event CredentialAddedByUser(string name,address indexed user);
+
+    // mapping of all files of user
+    mapping(address => FileStruct[]) private _allFilesOfUser; // addressOfUser => files[]
+
+    //  mapping of all credentials of user
+    mapping(address => CredentialStruct[]) private _allCredentialsOfUser; //addressOfUser => credentials[]
+
+    mapping(address => FileStruct[]) private _allSecretInfo; //addressOfUser => credentials[]
+
+
+
+    // add data of file of user
+    function addFileOfUser(FileStruct memory _fileData) external {
+        _allFilesOfUser[msg.sender].push(_fileData);
+    }
+
+    // add credential of user
+    function addScreatInfoOfUser(FileStruct memory _scretInfo) external {
+        _allSecretInfo[msg.sender].push(_scretInfo);
+    }
+
+    function addCredentialOfUser(
+        CredentialStruct memory _credentialData
+    ) external {
+        _allCredentialsOfUser[msg.sender].push(_credentialData);
+    }
+
+
+
+    // get total files count that user added
+    function userTotalFilesCount() internal view returns (uint256) {
+        return _allFilesOfUser[msg.sender].length;
+    }
+
+    // get total files count that user added
+    function userTotalScreatInfoCount() internal view returns (uint256) {
+        return _allSecretInfo[msg.sender].length;
+    }
+
+    // get total credentials count that user added
+    function userTotalCredentialCount() internal view returns (uint256) {
+        return _allCredentialsOfUser[msg.sender].length;
+    }
+
+
+
+    function getAllFilesOfUser()
+        external
+        view
+        returns (FileStruct[] memory allFiles)
+    {
+        uint256 totalFiles = userTotalFilesCount();
+        allFiles = new FileStruct[](totalFiles);
+        for (uint i = 0; i < totalFiles; i++) {
+            allFiles[i] = _allFilesOfUser[msg.sender][i];
+        }
+    }
+
+    function getAllCredentialsOfUser()
+        external
+        view
+        returns (CredentialStruct[] memory allCreds)
+    {
+        uint256 totalCredentials = userTotalCredentialCount();
+        allCreds = new CredentialStruct[](totalCredentials);
+        for (uint i = 0; i < totalCredentials; i++) {
+            allCreds[i] = _allCredentialsOfUser[msg.sender][i];
+        }
+    }
+
+    function getAllScreatInfoOfUser()
+        external
+        view
+        returns (FileStruct[] memory allInfos)
+    {
+        uint256 totalScreatInfo = userTotalScreatInfoCount();
+        allInfos = new FileStruct[](totalScreatInfo);
+        for (uint i = 0; i < totalScreatInfo; i++) {
+            allInfos[i] = _allSecretInfo[msg.sender][i];
+        }
+    }
+}
