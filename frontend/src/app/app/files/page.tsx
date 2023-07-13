3fb1d3dcc1e25df84824bf4db44d3fe3b94caa5a
abhithory <abhithory@gmail.com>
2023-07-13 14:52:29 +0530

    update upload data in pages

--- frontend/src/app/app/files/page.tsx
+++ frontend/src/app/app/files/page.tsx
@@ -1,18 +1,20 @@
+import UploadData from '@/components/Sections/Credentials/UploadData'
 import AllFilesList from '@/components/Sections/Files/AllFilesList'
 import UploadFiles from '@/components/Sections/Files/UploadFiles'
+import { DataTypeEnum } from '@/interfaces/DataInterface'
 import React from 'react'
 
 function page() {
   return (
     <main className='w-full'>
-    <section className='flex justify-end mt-6 mr-8'>
-        <UploadFiles />
-    </section>
-    <section className='mt-8'>
+      <section className='flex justify-end mt-6 mr-8'>
+        <UploadData type={DataTypeEnum.FILE} />
+      </section>
+      <section className='mt-8'>
         <h1 className='text_primary_gradient text-2xl mb-4 text-center'>All your Files</h1>
         <AllFilesList />
-    </section>
-</main>
+      </section>
+    </main>
   )
 }
 
--- frontend/src/app/app/page.tsx
+++ frontend/src/app/app/page.tsx
@@ -1,5 +1,6 @@
 import AllCredentialsList from '@/components/Sections/Credentials/AllCredentialsList'
-import UploadCredentails from '@/components/Sections/Credentials/UploadCredentails'
+import UploadData from '@/components/Sections/Credentials/UploadData'
+import { DataTypeEnum } from '@/interfaces/DataInterface'
 import React from 'react'
 
 
@@ -7,7 +8,7 @@ function page() {
     return (
         <main className='w-full'>
             <section className='flex justify-end mt-6 mr-8'>
-                <UploadCredentails />
+                <UploadData type={DataTypeEnum.CREDENTIALS} />
             </section>
             <section className='mt-8'>
                 <h1 className='text_primary_gradient text-2xl mb-4 text-center'>All your Credentials</h1>
