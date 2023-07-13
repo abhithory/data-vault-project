a8bccb3e8a7eb519ce4466b3a69b9ead5534376b
abhithory <abhithory@gmail.com>
2023-07-13 17:21:08 +0530

    small bug fixed

--- frontend/src/components/Sections/Credentials/CredentialsForm.tsx
+++ frontend/src/components/Sections/Credentials/CredentialsForm.tsx
@@ -14,7 +14,7 @@ interface CredentialsFormProps{
 function CredentialsForm({type,setCredentialsData, submitForm, credentialsData, uploadingCredential}:CredentialsFormProps) {
     return (
             <form onSubmit={submitForm} className='flex_center flex-col gap-4 mt-4'>
-                <NormalInput type="text" id="websiteUrl" name="websiteUrl" className='input_1' placeholder='Enter Website URL' value={credentialsData.websiteurl} onChange={(e)=>setCredentialsData({...credentialsData,websiteurl: e.target.value})} copy={type === "update"} required/>
+                <NormalInput type="text" id="websiteUrl" name="websiteUrl" className='input_1' placeholder='Enter Website URL' value={credentialsData.websiteurl} onChange={(e)=>setCredentialsData({...credentialsData,websiteurl: e.target.value, credentialName: e.target.value})} copy={type === "update"} required/>
                 <NormalInput type="text" id="userid" name="userid" className='input_1' placeholder='Enter User Id (Email/UserName/Phone)' value={credentialsData.userid} onChange={(e)=>setCredentialsData({...credentialsData,userid: e.target.value})} copy={type === "update"} required/>
                 <NormalInput type="password" id="pass" placeholder='Enter your password' value={credentialsData.password} onChange={(e)=>setCredentialsData({...credentialsData,password: e.target.value})} copy={type === "update"} required />
 
