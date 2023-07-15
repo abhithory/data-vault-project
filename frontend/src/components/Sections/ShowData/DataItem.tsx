a70139719152abe39a29e5714e896a77610d4b3b
abhithory <abhithory@gmail.com>
2023-07-15 08:54:51 +0530

    responsive data item

--- frontend/src/components/Sections/ShowData/DataItem.tsx
+++ frontend/src/components/Sections/ShowData/DataItem.tsx
@@ -3,6 +3,8 @@ import { DataExtendedInterface, DataTypeEnum } from '@/interfaces/DataInterface'
 import { getDateToStringFromSec, getDayTimeToStringFromSec } from '@/utils/Helper'
 import React from 'react'
 import { FaCreditCard } from 'react-icons/fa'
+import { RiLockPasswordFill } from 'react-icons/ri'
+import { PiFileLock } from 'react-icons/pi'
 
 interface CredentialWithFunctionInterface {
     type: DataTypeEnum,
@@ -24,7 +26,12 @@ function DataItem({
         <div className="relative group w-[14rem]">
             <div className="absolute -inset-0 bg-gradient-to-r from-primary via-third to-secondary rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
             <div className='flex_center flex-col border border-primary rounded-lg py-12 gap-2 glassmorphism-bg bg-primary-bg/90 cursor-pointer'>
-                <FaCreditCard className="text-2xl" />
+                {type === DataTypeEnum.CREDENTIALS ?
+                 <RiLockPasswordFill className="text-2xl" />
+                 : type === DataTypeEnum.FILE &&
+                <PiFileLock className="text-2xl" />
+                 }
+
                 <div className="flex_center w-[12rem]">
                     <h1 className='text-lg text-ellipsis overflow-hidden truncate'>{file.dataName}</h1>
                 </div>
