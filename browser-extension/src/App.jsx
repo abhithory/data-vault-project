77282f41f53fc59746d6193998f5da7790e38bca
abhithory <abhithory@gmail.com>
2023-07-15 14:16:56 +0530

    fixed

--- browser-extension/src/App.jsx
+++ browser-extension/src/App.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
+
 function App() {
   return (
     <div>
-      <h1>hello word</h1>
+      this is from app
     </div>
   );
 }
--- browser-extension/src/index.jsx
+++ browser-extension/src/index.jsx
@@ -5,7 +5,12 @@ import App from './App.jsx';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
+      <div>
+      <h1>
+     hlllo
+      </h1>
+    </div>
   </React.StrictMode>
 );
 
