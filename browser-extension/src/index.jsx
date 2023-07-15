f6ae939834135b50528097dbc4c743690fc70f52
abhithory <abhithory@gmail.com>
2023-07-15 14:55:45 +0530

    autoupdate added

--- browser-extension/src/index.jsx
+++ browser-extension/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
+// chrome-extension://fgdeknoakcekkhokkmhojcphjeopligi/index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
