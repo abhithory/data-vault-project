12ff38195f65218b13206e1e15f4a820a9c48350
abhithory <abhithory@gmail.com>
2023-07-15 13:55:03 +0530

    babel added for jsx

--- browser-extension/webpack.config.js
+++ browser-extension/webpack.config.js
@@ -9,4 +9,16 @@ module.exports = {
     // filename: 'my-first-webpack.bundle.js',
     filename: '[name].js',
   },
+  module: {
+    rules: [{ 
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/, 
+        use: {
+            loader: 'babel-loader',
+            options:{
+                presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+        }
+    }],
+  },
 };
