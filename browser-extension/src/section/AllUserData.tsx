7d7f512380210df4ab3ba970330b641b697fef91
abhithory <abhithory@gmail.com>
2023-07-16 18:26:26 +0530

    data item added

--- browser-extension/src/section/AllUserData.tsx
+++ browser-extension/src/section/AllUserData.tsx
@@ -1,9 +1,73 @@
+import { DataExtendedInterface, DataStructInterface, DataTypeEnum } from '../interfaces/DataInterface'
 import DataItem from './DataItem'
 
-function AllUserData() {
+interface UploadDataInterface {
+  type: DataTypeEnum
+}
+
+function AllUserData(props:UploadDataInterface) {
+  async function showDecryptedData(n: number) {
+    // try {
+    //   const _fullURL: string = await getFileUrlFromIpfsHash(dataArray[n].dataHash);
+    //   const _res = await fetch(_fullURL);
+    //   const encryptedFile = await _res.blob();
+    //   const decryptedFile: Blob = await decryptFile(encryptedFile, dataArray[n].decryptKey);
+    //   if (props.type === DataTypeEnum.CREDENTIALS) {
+    //     const formData: CredentialsFormData = JSON.parse(await unZipAndGetData(decryptedFile as File));
+    //     setCredentialsData(formData)
+    //     setShowDataModel(true)
+    //   } else if (props.type === DataTypeEnum.FILE) {
+    //     downloadFile(decryptedFile, dataArray[n].dataName + ".zip")
+    //   }
+    // } catch (error) {
+    //   console.log("showDecryptedData", error);
+
+    // }
+  }
+
+  async function handleDecryptData(n: number) {
+    // if (!address) return
+    // const _decryptedKey = await decryptMessage(dataArray[n].decryptKey, address);
+    // if (_decryptedKey) {
+    //   setDecryptKey(dataArray[n].id, _decryptedKey)
+    // }
+  }
+
+  const fileData:DataExtendedInterface = {
+    dataName: "google.com",
+    dataType: DataTypeEnum.CREDENTIALS,
+    dataHash: "string",
+    decryptKey: "string",
+    uploadTime: (new Date()).getTime() / 1000,
+    id: "string",
+    decryptedStatus: false,
+  }
   return (
-    <div className='flex flex-wrap flex-col h-full'>
-      {/* <DataItem /> */}
+    <div className='flex flex-wrap flex-col h-full w-full px-2'>
+       <DataItem
+                key={0}
+                index={0}
+                type={props.type}
+                file={fileData}
+                showDecryptedData={showDecryptedData}
+                handleDecryptData={handleDecryptData}
+              />
+                     <DataItem
+                key={0}
+                index={0}
+                type={props.type}
+                file={fileData}
+                showDecryptedData={showDecryptedData}
+                handleDecryptData={handleDecryptData}
+              />
+                     <DataItem
+                key={0}
+                index={0}
+                type={props.type}
+                file={fileData}
+                showDecryptedData={showDecryptedData}
+                handleDecryptData={handleDecryptData}
+              />
     </div>
   )
 }
--- browser-extension/src/section/DataItem.tsx
+++ browser-extension/src/section/DataItem.tsx
@@ -2,7 +2,6 @@ import NormalButton from '../components/Buttons/NormalButton'
 import { DataExtendedInterface, DataTypeEnum } from '../interfaces/DataInterface'
 import { getDateToStringFromSec, getDayTimeToStringFromSec } from '../utils/Helper'
 import { useState } from 'react'
-import { FaCreditCard } from 'react-icons/fa'
 import { RiLockPasswordFill } from 'react-icons/ri'
 import { PiFileLock } from 'react-icons/pi'
 
@@ -23,39 +22,39 @@ function DataItem({
     const [isDownloading, setIsDownloading] = useState(false);
     const [isDecrypting, setIsDecrypting] = useState(false);
 
-    async function showData(){
+    async function showData() {
         setIsDownloading(true)
         try {
             await showDecryptedData(index);
         } catch (error) {
-            console.log("showData",error);
-        } finally{
+            console.log("showData", error);
+        } finally {
             setIsDownloading(false)
         }
     }
 
 
-    async function decryptData(){
+    async function decryptData() {
         setIsDecrypting(true)
         try {
             await handleDecryptData(index);
         } catch (error) {
-            console.log("decryptData",error);
-        } finally{
+            console.log("decryptData", error);
+        } finally {
             setIsDecrypting(false)
         }
     }
     return (
-        <div className="relative group w-[14rem]">
-            <div className="absolute -inset-0 bg-gradient-to-r from-primary via-third to-secondary rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
-            <div className='flex_center flex-col border border-primary rounded-lg py-12 gap-2 glassmorphism-bg bg-primary-bg/90 cursor-pointer'>
-                {type === DataTypeEnum.CREDENTIALS ?
-                 <RiLockPasswordFill className="text-2xl" />
-                 : type === DataTypeEnum.FILE &&
-                <PiFileLock className="text-2xl" />
-                 }
+        <div className="w-full">
 
-                <div className="flex_center w-[12rem]">
+            <div className='flex_center border border-primary rounded-lg gap-2 glassmorphism-bg  cursor-pointer text-text-color'>
+                {type === DataTypeEnum.CREDENTIALS ?
+                    <RiLockPasswordFill className="text-2xl" />
+                    : type === DataTypeEnum.FILE &&
+                    <PiFileLock className="text-2xl" />
+                }
+
+                <div className="flex_center">
                     <h1 className='text-lg text-ellipsis overflow-hidden truncate'>{file.dataName}</h1>
                 </div>
                 <div className="text-center">
@@ -75,7 +74,7 @@ function DataItem({
                         className="btn_primary_2 text-sm px-4 py-2"
                         text={type === DataTypeEnum.CREDENTIALS ? "Decrypt Credentials" : type === DataTypeEnum.FILE ? "Decrypt File" : ""}
                         loading={isDecrypting}
-                    
+
                     />
                 }
             </div>
